ba237ddf88c680e06a66861cb6c8416409ecba3c
Zach Gavin <email@email.com>
2018-04-21 15:13:44 -0400

    Adding github pages configuations.

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
     author: "Aman Mittal",
     description: "A Gatsby.js Starter based on Ceevee by Styleshout"
   },
-  pathPrefix: "/",
+  pathPrefix: "/caseyportfoliopage",
   plugins: [
     {
       resolve: `gatsby-plugin-typography`,
