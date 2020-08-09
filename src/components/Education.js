719e869ed32d672346fea3801de4252cff663a69
Casey Ferrara <caseyferrara@Caseys-MacBook-Pro.local>
2020-08-09 15:32:41 -0500

    Add redirect index.html

--- src/components/Education.js
+++ src/components/Education.js
@@ -15,26 +15,26 @@ const Education = () => (
           <p className="info" id="main-label">
             Bachelor of Arts in Psychology & Linguistics
             <span>&bull;</span>
-            <em className="date">June 2014</em>
+            <em className="date">2014</em>
           </p>
 
           <ul>
-            <li> Thesis: "Only Some Unicorns are Ugly: How People Understand Different Types of Meaning"</li>
+            <li> Undergraduate Thesis (2014): "Only Some Unicorns are Ugly: How People Understand Different Types of Meaning"</li>
             <li style={{fontStyle: 'italic'}}>Senior project using eye-tracking to evaluate the time course of verification when listeners are provided an utterance that includes a scalar implicature. </li>
           </ul>
         </div>
 
         <div className="twelve columns">
-          <h3>Swarthmore College</h3>
+          <h3>University of Chicago</h3>
           <p className="info" id="main-label">
-            Bachelor of Arts in Psychology & Linguistics
+            PhD, Psychology
             <span>&bull;</span>
-            <em className="date">June 2014</em>
+            <em className="date">Expected completion in 2023</em>
           </p>
 
           <ul>
-            <li> Thesis: "Only Some Unicorns are Ugly: How People Understand Different Types of Meaning"</li>
-            <li style={{fontStyle: 'italic'}}>Senior project using eye-tracking to evaluate the time course of verification when listeners are provided an utterance that includes a scalar implicature. </li>
+            <li> Master's Thesis (2020): "From Frozen to Flexible: Iconic Modification in ASL"</li>
+            <li style={{fontStyle: 'italic'}}>Second year master's project exploring (1) whether ASL signers iconically modify supposedly "frozen" lexeme signs, and (2) to what extent these modifications are constrained by linguistic and non-linguistic factors. </li>
           </ul>
         </div>
       </div>
